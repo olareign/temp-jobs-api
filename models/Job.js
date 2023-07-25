ecb9fc1ce0c1c4f2e48faa2258da540c91aa9426
olareign <abdulrasaqolarewaju88@gmail.com>
2023-07-25 15:32:01 +0100

    first commit

---
+++ models/Job.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+
+const JobSchema = new mongoose.Schema({
+    company: {
+        type: String,
+        required: [true, 'Please provide company name'],
+        maxlength:50
+    },
+    position:{
+        type: String,
+        required: [true, 'Please provide company position'],
+        maxlength:100
+    },
+    status:{
+        type: String,
+        enum:['interview','decline','pending'],
+        default:'pending',
+    },
+    createdBy: {
+        type:mongoose.Types.ObjectId,
+        ref:'User',
+        required:[true, 'Please provide a valid user']
+    }
+},{timestamps:true})
+
+
+module.exports = mongoose.model('Job', JobSchema)
